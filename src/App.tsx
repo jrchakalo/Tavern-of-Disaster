f1a6d28501cf6f7c53ccf3049eb5fafdb28fdfc2
jrchakalo <jisj@cin.ufpe.br>
2024-08-28 00:57:05 -0300

    refactor: add elements to a global css

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import './global.css';
 import { Route, Routes } from 'react-router-dom';
 import './App.css'
 import Dice from './pages/Dice.tsx'
