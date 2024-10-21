d21bd92565824f36fb0ce07502af6938f8a86dc2
jrchakalo <jisj@cin.ufpe.br>
2024-10-20 22:21:17 -0300

    refactor: some changes to merge process

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,13 @@ import './global.css';
 import { Route, Routes } from 'react-router-dom';
 import './App.css'
 import Dice from './pages/Dice.tsx'
-import Login from './pages/Login.tsx'
+{/*import Login from './pages/Login.tsx'
 import Signup from './pages/Signup.tsx'
 import MyTables from './pages/MyTables.tsx'
 import Table from './pages/Table.tsx'
 import ProtectedRoute from './components/ProtectedRoute.tsx';
 import ResetPassword from './pages/ResetPassword.tsx';
-import RequestResetPassword from './pages/RequestResetPassword.tsx';
+import RequestResetPassword from './pages/RequestResetPassword.tsx';*/}
 import Home from './pages/Home.tsx';
 import { AuthProvider } from './context/AuthContext.tsx';
 import BattleMap from './pages/BattleMap.tsx';
@@ -20,7 +20,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/home" element={<Home />} />
         <Route path="/dice" element={<Dice />} />
-        <Route path="/login" element={<Login />} />
+        {/*<Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route
               path="/my-tables"
@@ -32,7 +32,7 @@ function App() {
             />
         <Route path="/:tableCode" element={<Table />} />
         <Route path="/reset-password/:token" element={<ResetPassword />} />
-        <Route path="/request-reset-password" element={<RequestResetPassword />} />
+        <Route path="/request-reset-password" element={<RequestResetPassword />} />*/}
         <Route path="/battle-map" element={<BattleMap />} />
       </Routes>
     </AuthProvider>
